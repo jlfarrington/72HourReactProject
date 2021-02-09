ef0fcafa4ca993c061fee77b480c6d1b311dc57f
Jenn Farrington <jlfarrington0@gmail.com>
2021-02-08 21:18:51 -0500

    styled weather

--- teamReactApp/src/components/Nasa.js
+++ teamReactApp/src/components/Nasa.js
@@ -1,21 +1,21 @@
-import React,{useEffect} from 'react';
+// import React from 'react';
 
 
 
-const Nasa = ({ lat, long }) => {
-  console.log(lat, long);
+// const Nasa = ({ lat, long }) => {
+//   console.log(lat, long);
 
-  const key = 'tbatxuwbbitdx9d24pHWVcijgTtUWP6EVXiQNVN4'
-  const url = `https://api.nasa.gov/planetary/earth/imagery?lon=${long}&lat=${lat}&date=2018-02-07&dim=0.025&api_key=${key}`;
+//   const key = 'tbatxuwbbitdx9d24pHWVcijgTtUWP6EVXiQNVN4'
+//   const url = `https://api.nasa.gov/planetary/earth/imagery?lon=${long}&lat=${lat}&date=2018-02-07&dim=0.025&api_key=${key}`;
 
 
   
-  return (
-    <div>
-      <p>Here's a picture of where you are!</p>
-      {url ? <img src={url} width="400px" height="400px" /> : <p>no image found</p>}
-      </div>
-  );
-};
+//   return (
+//     <div>
+//       <p>Here's a picture of where you are!</p>
+//       {url ? <img src={url} width="400px" height="400px" /> : <p>no image found</p>}
+//       </div>
+//   );
+// };
 
-export default Nasa;
+// export default Nasa;
